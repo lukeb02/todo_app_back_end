b765d3da3edd30fd0a94d0c995eb3fa408c6aedb
Luke Brusseau <lbrusseau@outlook.com>
2018-07-11 17:44:24 +0000

    Change2

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,7 +4,14 @@ end
 def show
 @todo_description = "Eat eggs"
 @todo_pomodoro_estimate = "7"
-
+todo_id = params[:id]
+if todo_id == '1'
+    @todo_description = 'Im Changed' 
+    @todo_pomodoro_estimate = '100'
+    elsif todo_id == '5'
+    @todo_description = 'Hello'
+    @todo_pomodoro_estimate = '10'
+end
 end
 
 end
